8f4e835f9c0d5d4fdc56a33a57357af6cdf633db
newton.mallick <newton.mallick@celebaltech.com>
2024-12-11 13:20:36 +0530

    Updated Program.cs

--- Program.cs
+++ Program.cs
@@ -1,107 +1,79 @@
-﻿
-using Tensorflow.NumPy;
-using Tensorflow;
-using Tensorflow.Keras;
+﻿using Tensorflow;
+using Tensorflow.Util;
 
 const int ImgH = 100;
 const int ImgW = 100;
-const int NChannels = 1;//3;
+const int NChannels = 1; //3;
 
-Console.Write(ImgH);
+//Console.Write(ImgH);
 Console.WriteLine(Directory.GetCurrentDirectory());
 
 var basePath = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory,"..","..","..", "Dataset"));
 Console.WriteLine(basePath);
 Console.WriteLine(AppContext.BaseDirectory);
-var result = Directory.GetDirectories(basePath);
 
-foreach (var x in result)
-{
-    Console.WriteLine(x);
-}
+//var result = Directory.GetDirectories(basePath);
+//foreach (var x in result){Console.WriteLine(x);}
 
-List<int> yLabels = [];
-List<string> xImagePath = [];
-var fO = new FileOperation();
+var fO = new FileOperation(ImgH,ImgW,NChannels);
 
 // Train Data
 var trainPath = Path.Combine(basePath, "Train");
 var testPath = Path.Combine(basePath, "Test");
 var validatePath = Path.Combine(basePath, "Validation");
 
-Dictionary<string, string[]> dict = fO.GetImagePath(trainPath);
-
-var  xTrain = dict.ToDictionary(
-    x=>x.Key, 
-    x=>np.zeros((x.Value.Length, ImgH, ImgW, NChannels), dtype: TF_DataType.TF_FLOAT)
-    );
-
-foreach (var k in dict.Keys)
-{
-    fO.LoadImage(dict[k], xTrain[k],"Training");
-}
-
-
-//foreach (var x in dict)
-//{
-//    xTrain.Add(x.Key, np.zeros((x.Value.Length, ImgH, ImgW, NChannels), dtype: TF_DataType.TF_FLOAT));
-//}
-
-var p = new Preprocessing();
-
 Console.WriteLine("TrainDB");
-var train_db =p.image_dataset_from_directory(trainPath,
-    validation_split: 0.0f,
-    //label_mode:"binary", // int , cate
-    label_mode:"categorical",
-    subset: "training",
-    //color_mode:"grayscale",
-    seed: 111,
-    image_size: (ImgH,ImgW)
-    ,batch_size:64
-    );
+var (xTrain,yTrain)  = fO.LoadFromDir(trainPath,"Training");
 Console.WriteLine("TestDB");
-var test_db = p.image_dataset_from_directory(testPath,
-    validation_split: 0.0f,
-    label_mode: "binary",
-    subset: "training",
-    seed: 222,
-    //color_mode: "grayscale",
-    image_size: (ImgH, ImgW)
-    ,batch_size:16
-);
+var (xTest, yTest)   = fO.LoadFromDir(testPath, "Test");
 Console.WriteLine("ValidDB");
-var validate_db = p.image_dataset_from_directory(validatePath,
-    //validation_split: 0.0f,
-    label_mode: "binary",
-    subset: "validation", 
-    //color_mode: "grayscale",
-    seed: 333,
-    image_size: (ImgH, ImgW)
-    ,batch_size:8
-);
+var (xValid, yValid) = fO.LoadFromDir(validatePath, "Valid");
+
+//var img = xTrain.numpy()[0];
+//fO.CreateImage(img,Path.Combine(basePath,"Image.jpeg"));
 
 var m = new FaceMaskModel();
 
-m.BuildModel(ImgH,ImgW,NChannels,2);
-m.Train(train_db, validate_db);
+// Binary Classifier Model ClassNumber as 1 as 
+m.BuildModel(ImgH,ImgW,NChannels,1);
 m.Compile();
-//Create Empty
-// TF message coming from here
-//var xTrain =  np.zeros((records.Count, ImgH, ImgW, NChannels), dtype: tf.float32); // TotalRecords * Height * width * Channel
-//var yTrain = tf.one_hot(np.array(xLabels.ToArray(),dtype:tf.int64), depth: classCount);
-//var yTrain = np.eye(classCount, dtype: tf.float32)[np.array(yLabels.ToArray(), tf.float32).reshape(-1)];
-// Encode label to a one hot vector.
 
-//var indexArray = np.array(xLabels.ToArray());  // N * xLabels.Total
+Console.WriteLine("Training Started....");
+m.Train(xTrain, yTrain,20,new ValidationDataPack((xValid,yValid)));
 
-//var one = yTrain[indexArray];
+Console.WriteLine("Summary");
+m.Summary();
+Console.WriteLine();
 
-//indexArray = indexArray.reshape(-1);
+var predict = m.Predict(xTest);
 
-//var one_hot_targets = np.eye(uniqueLabels.Length)[indexArray];
-//var sh = one_hot_targets.shape;
-//Load labels
+foreach (var p in predict)
+{
+    Console.WriteLine(p);
+}
+
+List<List<int>> matrix = new();
+
+for (int ind = 0; ind < 2; ind++)
+{
+    matrix.add(Enumerable.Repeat(0, 2).ToList());
+}
+Console.WriteLine();
+
+//for (int i = 0; i < 2; i++)
+//{
+//    try
+//    {
+//        int j = yTest.numpy()[i];
+//        matrix[predict.numpy()[i]][j] += 1;
+//    }
+//    catch (Exception ex)
+//    {
+//        Console.WriteLine();
+//    }
+//}
+Console.WriteLine();
 
 
 Console.ReadLine();
+
